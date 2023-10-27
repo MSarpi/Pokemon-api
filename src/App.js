479747241635508e9b528afc2d6fd7a37e73f7a3
MSarpi <miguel.sarpi@gmail.com>
2023-10-26 22:15:17 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Buscador from './component/componentes/Buscador';
+import CardPokemon from './component/componentes/CardPokemon';
+
+
 import './App.css';
+import '../src/component/css/buscador.css'
+import '../src/component/css/cardpokemon.css'
+import '../src/component/css/modal_resultado_buscador.css'
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="logo_pokemon">
+          <div className='row'>
+            <div className='col-3'>
+            </div> 
+            <div className='col-6'>
+              <div className="overlay">
+              </div>
+            </div>
+            <div className='col-3'>
+
+            </div> 
+          </div>
+        </div>
+        <Buscador/>
+        
       </header>
+      <div class="container-fluid bg-secondary altura"></div>
+      <CardPokemon/>
     </div>
   );
 }
